15e56941a1bb2f333a25931675cfc4a832f18d01
Ryan Riddiford <menialcub211@gmail.com>
2023-08-03 18:35:46 +1000

    Initial push containing completed Music Chat app

---
+++ Helpers/AWSConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace music_chat.Helpers
+{
+
+    /// <summary>
+    /// AWS constants for using AWS S3 bucket
+    /// </summary>
+    public class AWSConstants
+    {
+        public const string S3_BUCKET_URL = "https://myapp-a3-cloud.s3.ap-southeast-2.amazonaws.com/";
+
+        public const string AWS_BUCKET_NAME = "myapp-a3-cloud";
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace music_chat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
